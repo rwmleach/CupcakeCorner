372a6da3f2fb8af638dfa74d71a7f8a16fd6a691
Ryan Leach <rwmleach@gmail.com>
2019-12-05 09:27:52 -0500

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Ryan Leach on 12/5/19.
+//  Copyright © 2019 Ryan Leach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
